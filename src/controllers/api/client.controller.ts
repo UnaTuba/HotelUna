fa3b790df5ea641617ee3f4cf79c5e4b88da76b5
EvaTuba <eva.tuba@gmail.com>
2020-06-19 23:15:18 +0200

    Test via postman, create ApiResponse

--- src/controllers/api/client.controller.ts
+++ src/controllers/api/client.controller.ts
@@ -3,6 +3,7 @@ import { ClientService } from "../../services/client/client.service";
 import { Client } from "../../../entities/client.entity";
 import { AddClientDto } from "../../dtos/client/add.client.dto";
 import { EditClientDto } from "../../dtos/client/edit.client.dto";
+import { ApiResponse } from "../misc/api.response.class";
 
 @Controller('api/client')
 export class ClientController {
@@ -16,16 +17,24 @@ export class ClientController {
     }
 
     @Get(':id') // http://localhost:3000/api/client/4/
-    getById( @Param('id') clientId: number): Promise<Client> {
-        return this.clientService.getById(clientId);
+    async getById( @Param('id') clientId: number): Promise<Client | ApiResponse> {
+        return new Promise(async (resolve) => {
+            const clientOne = await this.clientService.getById(clientId);
+
+            if (clientOne === undefined){
+                resolve(new ApiResponse("error",-1002));
+            }
+
+            resolve(clientOne);
+        });
     }
 
     @Put('') //PUT http://localhost:3000/api/client/
-    add( @Body() data: AddClientDto): Promise<Client>{
+    add( @Body() data: AddClientDto): Promise<Client | ApiResponse>{
         return this.clientService.add(data);
     }
     @Post(':id') //POST http://localhost:3000/api/client/4
-    edit( @Param('id') clientId: number, @Body() data: EditClientDto): Promise<Client> {
+    edit( @Param('id') clientId: number, @Body() data: EditClientDto): Promise<Client | ApiResponse> {
         return this.clientService.editById(clientId,data);
     }
 }
--- src/services/client/client.service.ts
+++ src/services/client/client.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 import { Client } from '../../../entities/client.entity';
 import { AddClientDto } from '../../dtos/client/add.client.dto';
 import { EditClientDto } from '../../dtos/client/edit.client.dto';
+import { ApiResponse } from 'src/controllers/misc/api.response.class';
+import { resolve } from 'dns';
 
 
 @Injectable()
@@ -17,12 +19,12 @@ export class ClientService {
         return this.client.find();
     }
 
-    getById(id: number): Promise<Client> {
+    getById(id: number): Promise<Client | ApiResponse> {
         return this.client.findOne(id);
     }
 
     //add
-    add(data: AddClientDto): Promise<Client>{
+    add(data: AddClientDto): Promise<Client | ApiResponse>{
         // DTO      => Model
         // username -> username; itd
         const newClient: Client = new Client();
@@ -31,7 +33,14 @@ export class ClientService {
         newClient.email = data.email;
         newClient.phone = data.phone;
 
-        return this.client.save(newClient);
+        return new Promise((resolve) =>{
+            this.client.save(newClient)
+            .then(data => resolve(data))
+            .catch(error => {
+                const response: ApiResponse = new ApiResponse("error",-1001);
+                resolve(response);
+            });
+        });
         
         //U USER>SERVICE!!!! password is special (needed in user!!!)
         //import * as crypto from "crypto";
@@ -40,8 +49,14 @@ export class ClientService {
     }
 
     //editById
-    async editById(id: number, data: EditClientDto): Promise<Client>{
+    async editById(id: number, data: EditClientDto): Promise<Client | ApiResponse>{
         const newClient: Client = await this.client.findOne(id);
+        
+        if (newClient === undefined) {
+            return new Promise((resolve) => {
+                resolve(new ApiResponse("error",-1002));
+            });
+        }
 
         newClient.surname = data.surname;
         newClient.phone = data.phone;
